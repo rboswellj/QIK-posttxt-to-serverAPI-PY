af51e1e08dbe64fe65e8712b33b267e498846b86
Robert Johnson <rboswellj@gmail.com>
2025-08-06 23:06:14 -0400

    Feedback is now properly posting to the server

--- run.py
+++ run.py
@@ -30,6 +30,10 @@ def test_read_reviews(review_directory):
 
 def read_reviews(directory):
     reviews = []
+    # Read all text files in the specified directory and append dictionart to list 'reviews'
+    if not os.path.exists(directory):
+        print(f"Directory {directory} does not exist.")
+        return reviews
     for filename in os.listdir(directory):
         if filename.endswith('.txt'):
             with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
@@ -46,31 +50,27 @@ def read_reviews(directory):
                         reviews.append(review)
     return reviews
 
-def convert_to_json(reviews):
-    return json.dumps(reviews, ensure_ascii=False)
-
-def post_reviews(url, reviews):
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=reviews, headers=headers)
-    return response
+def post_reviews(url, reviews_dictionary):
+    # Post each review dictionary to the specified URL
+    for item in reviews_dictionary:
+        response = requests.post(url, json=item)
+        if response.status_code != 201:
+            raise Exception('POST error status={}'.format(response.status_code))
+        print('Created feedback ID: {}'.format(response.json()["id"]))
+            
 
 def main():
-    review_directory = './data/feedback'  # Replace with the actual path to your review directory
-    # url = 'http://example.com/api/reviews'  # Replace with the actual URL
+    review_directory = '/data/feedback'  # Replace with the actual path to your review directory
+    url = 'http://example.com/feedback'  # Replace with the actual URL
 
     #Test functions: Can be commented out after testing
-    test_read_reviews(review_directory)
+    #test_read_reviews(review_directory)
+    
+    # Read in reviews and send to post request. Return response code
+    reviews = read_reviews(review_directory)
+    post_reviews(url, reviews)
 
-    # Uncomment the following lines to post reviews to the server
-    '''
-    reviews_json = convert_to_json(reviews)
-    response = post_reviews(url, reviews_json)
-
-    if response.status_code == 200:
-        print("Reviews posted successfully:", response.json())
-    else:
-        print("Failed to post reviews. Status code:", response.status_code, "Response:", response.text)
-    '''
+    
 
 if __name__ == '__main__':
     main()  
